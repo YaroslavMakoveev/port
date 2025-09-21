cd1249f4978049180e619f5143dc9e084edf8b3f
YaroslavMakoveev <y-makoveev@mail.ru>
2025-09-21 23:41:46 +0300

    deploy

---
+++ script.js
@@ -0,0 +1,18 @@
+// Dog Explorer
+document.getElementById("dogGit").onclick = function () {
+  window.open("https://github.com/YaroslavMakoveev/dogs_react_pet", "_blank");
+};
+document.getElementById("dogDemo").onclick = function () {
+  window.open("https://yaroslavmakoveev.github.io/dogs_react_pet/", "_blank");
+};
+
+// Fake Store Shop
+document.getElementById("storeGit").onclick = function () {
+  window.open("https://github.com/YaroslavMakoveev/store_react_pet", "_blank");
+};
+document.getElementById("storeDemo").onclick = function () {
+  window.open(
+    "https://yaroslavmakoveev.github.io/store_react_pet/#/",
+    "_blank"
+  );
+};
